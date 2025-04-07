a891c62a035c88de2c6698e9e0eb9556aad9b1c6
dev-indre <slezaiteindre@gmail.com>
2025-04-07 11:33:42 +0300

    Title section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import Title from "./Components/Title";
 import TopBanner from "./Components/TopBanner";
 
 
@@ -9,7 +10,7 @@ export default function App() {
    <TopBanner/>
    <Header/>
    <Hero/>
-  
+   <Title text1="Shop" text2="By category" showLink={true}/>
    </>
   )
 }
---
+++ src/Components/Title.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Title({text1, text2, showLink}) {
+  return (
+    <div className="title-box">
+        <h2 className="title">
+            <span className="green-text">{text1}</span><span>{text2}</span>
+        </h2>
+        {showLink && (
+            <a className="title-link" href="#">Shop all &gt;</a>
+        )}
+    </div>
+  )
+}
